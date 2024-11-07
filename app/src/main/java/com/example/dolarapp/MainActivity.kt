c9d98037aaf2f29fa818a74d8696651e0c0fc209
Jose Perez Huanca <perezhuancajose@gmail.com>
2024-11-07 16:48:48 -0300

    Dropdown

--- app/src/main/java/com/example/dolarapp/MainActivity.kt
+++ app/src/main/java/com/example/dolarapp/MainActivity.kt
@@ -10,13 +10,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.heading
@@ -39,6 +48,9 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App(){
+    val itemsOption= listOf("Oficial","Blue","Tarjeta")
+    var dropdownExpanded by remember { mutableStateOf(false) }
+    var selected by remember { mutableStateOf(itemsOption[0]) }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -71,10 +83,34 @@ fun App(){
                 ){
                     Button(
                         onClick = {
-                            //Acción dropDown
+                            //Acción dropdown
+                            dropdownExpanded=true
                         }
                     ){
-                        Text("Oficial")
+                        Text(selected)
+                        Icon(
+                            imageVector = Icons.Default.ArrowDropDown,
+                            contentDescription = "Menú",
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = dropdownExpanded,
+                        onDismissRequest = {
+                            dropdownExpanded=false
+                        }
+                    ){
+                        for(option in itemsOption){
+                            DropdownMenuItem(
+                                text = {
+                                    Text(option)
+                                },
+                                onClick = {
+                                    selected=option
+                                    dropdownExpanded=false
+                                }
+                            )
+                        }
                     }
                     Button(
                         onClick = {
